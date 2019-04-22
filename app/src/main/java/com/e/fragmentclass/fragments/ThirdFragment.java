bef22c3e9ca5eb53dc08f938196367b4aea58969
sailesh limbu <saileshlimbu04@gmail.com>
2019-04-22 10:59:40 +0545

    Added viewpager

--- app/src/main/java/com/e/fragmentclass/fragments/ThirdFragment.java
+++ app/src/main/java/com/e/fragmentclass/fragments/ThirdFragment.java
@@ -15,15 +15,9 @@ import com.e.fragmentclass.R;
 public class ThirdFragment extends Fragment {
 
 
-    public ThirdFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_third, container, false);
 
         return view;
